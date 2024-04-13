6839454b9d5a4b1a520fc46f455cea856a88c862
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2024-04-13 07:20:20 +0100

    cors config

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import userRouter from "./routes/user.routes.js";
 import fruitRouter from "./routes/fruit.routes.js";
 
 const app = express();
 const port = 3000;
+
+
+
+app.use(cors({origin: "*",credentials: true, // Important for cookies, authorization headers with HTTPS
+methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], allowedHeaders: ["Origin",
+"Content-Type", "Accept", "Authorization", "X-Request-With"]}));
 
 
 app.use(bodyParser.json());
