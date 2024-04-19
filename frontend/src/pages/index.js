a8c7ad14bfe9e672c8af5c6bd7fb739818ebe602
Bibest <bibhestee@gmail.com>
2024-04-19 20:36:35 +0100

    protect cart and checkout page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,3 +10,4 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as ShopDetail } from './ShopDetail';
 export { default as Shop } from './Shop';
+export { default as Checkout } from './Checkout';
