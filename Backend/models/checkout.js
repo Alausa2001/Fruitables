258be95b90799699d32ce4e085da282220521892
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2024-04-21 09:24:31 +0100

    checkout done

--- Backend/models/checkout.js
+++ Backend/models/checkout.js
@@ -6,7 +6,6 @@ import { Cart } from './cart.js';
 
 const checkoutSchema = new Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: User },
-    cart: { type: mongoose.Schema.Types.ObjectId, ref: Cart },
     firstname: String,
     lastname: String,
     address: String,
