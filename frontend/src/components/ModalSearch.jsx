6dbd3df2a4413cba9b4abff3bd5efd4e9f345104
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-04-23 19:40:47 +0000

    Pending changes exported from your codespace

--- frontend/src/components/ModalSearch.jsx
+++ frontend/src/components/ModalSearch.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 const ModalSearch = () => {
+
+  const [searchInput, setSearchInput] = useState("");
+  const { allProducts } = useSelector(state => state.product);
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+
+    if (searchInput.length > 0) {
+      const found = allProducts.filter((product) => {
+        console.log(product)
+        return product.name.includes(searchInput) || product.category.includes(searchInput)
+      })
+      setItems(found);
+    }
+  };
+
+
   return (
     <div
       className="modal fade"
@@ -27,12 +50,53 @@ const ModalSearch = () => {
                 className="form-control p-3"
                 placeholder="keywords"
                 aria-describedby="search-icon-1"
+                onChange={handleChange}
               />
               <span id="search-icon-1" className="input-group-text p-3">
                 <i className="fa fa-search"></i>
               </span>
             </div>
           </div>
+          <div className="col-lg-9" style={{height: '100%'}}>
+              <div className="row g-4 justify-content-center" style={{height: '100%'}}>
+                {items.map((product) => (
+                  <div key={product._id} className="col-md-6 col-lg-6 col-xl-4">
+                    <div className="rounded position-relative fruite-item">
+                      <div className="fruite-img">
+                        <img
+                          src={product.imageUrl}
+                          className="img-fluid w-100 rounded-top"
+                          alt=""
+                        />
+                      </div>
+                      <div
+                        className="text-white bg-secondary px-3 py-1 rounded position-absolute"
+                        style={{ top: "10px", left: "10px" }}
+                      >
+                        {product.category}
+                      </div>
+
+                      <div className="p-4 border border-secondary border-top-0 rounded-bottom">
+                        <h4>{product.name}</h4>
+                        <p>{product.description}</p>
+                        <div className="d-flex justify-content-between flex-lg-wrap">
+                          <p className="text-dark fs-5 fw-bold mb-0">
+                            ${product.price} / kg
+                          </p>
+                          <Link
+                            to={`/product/${product._id}`}
+                            className="btn border border-secondary rounded-pill px-3 text-primary"
+                          >
+                            <i className="fa fa-eye me-2 text-primary"></i>{" "}
+                            View
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
         </div>
       </div>
     </div>
